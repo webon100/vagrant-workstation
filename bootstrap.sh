824a6642b2ae105587415de4045db23cf3219a12
webon <webon100@users.noreply.github.com>
2017-05-01 12:15:40 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -148,7 +148,10 @@ sudo touch /home/vagrant/.zshrc
 # wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 # sh -c "$(wget --no-check-certificate https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# chsh: user 'vagrant' does not exist ???
 sudo chsh -s /bin/zsh vagrant
+
 sudo zsh
 sudo chown vagrant:vagrant /home/vagrant/.zshrc
 sudo chown -R vagrant:vagrant /home/vagrant/.oh-my-zsh
